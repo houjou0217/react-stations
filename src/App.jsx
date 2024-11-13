a7352c565072a3a72b20728158ee75f970f3d0d4
Shinnosuke.Hojo <shinnosuke.houjou0217@gmail.com>
2024-11-13 23:20:00 +0900

    20241113

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-// DO NOT DELETE
+// DO NOT DELETß
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://dog.ceo/api/breeds/image/random")
+  const next_dog = () => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogアプリ</header>
+      <img src={dogUrl} alt='dog_img'></img>
+      <button onClick={next_dog}>更新</button>
     </div>
+    
   )
 }
